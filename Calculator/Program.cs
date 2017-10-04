c1dd021ea035ba88077dfe76aa72d770efa370a2
Guzel <guzel.r87@mail.ru>
2017-10-04 19:10:47 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+Задание: найти значение арифметического выражения, 
+не используя переменные
+      25 + 689
+12 x ---------- - 12 x 3
+       60 - 9 
+ */
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(12 * ((25 + 689) / (60 - 9)) - (12 * 3));
+            Console.ReadKey();
+        }
+    }
+}
